745277ca64033ec42402d9efe78a730dac59183a
Thân Văn Hiếu <vanhieuu99@gmail.com>
2022-06-21 10:25:56 +0700

    commit code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CoIcon from "./src/components/Icon";
 
-export default function App() {
+
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden  />
+        <Text>Hello</Text>
+        <CoIcon  name="user" family="AntDesign" color={"rgb(100,120,40)"} size={10}     />
     </View>
   );
 }
-
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:'center'
   },
 });
---
+++ src/components/Icon.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { ReactNode } from "react";
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import argonConfig from "../../assets/config/argon.json";
+import * as SplashScreen from "expo-splash-screen";
+import { IconFamilyType, Icon } from "galio-framework";
+const ArgonExtra = require("../../assets/font/argon.ttf");
+const IconArgonExtra = createIconSetFromIcoMoon(
+  argonConfig,
+  "ArgonExtra",
+  "argon.ttf"
+);
+
+interface IconProps {
+  name: string;
+  family: IconFamilyType | "ArgonExtra";
+  size: number;
+  color: string;
+}
+
+const CoIcon = ({ name, family, size, color, ...rest }: IconProps) =>{
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({ArgonExtra:ArgonExtra});
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoading(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (loading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loading]);
+
+  if (!loading) {
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <>
+        {family === "ArgonExtra" ? (
+          <IconArgonExtra
+          key={1}
+            name={name}
+            family={family}
+            {...rest}
+            size={size}
+            color={color}
+            style={{ marginRight: 5 }}
+          />
+        ) : (
+          <Icon
+            name={name}
+            family={family}
+            size={size}
+            color={color}
+            style={{
+              marginRight: 5,
+            }}
+          />
+        )}
+      </>
+    </View>
+  );
+};
+
+export default CoIcon;
+
+
